141e99badef18d2f08e55b648e711b84d1c17670
goudajyoti226 <goudajyoti226@gmail.com>
2024-07-05 20:43:35 +0530

    Automation of valid login testcase

--- test_scripts/test_script1.py
+++
@@ -1,9 +0,0 @@
-from generic.base_class import BaseClass
-from generic.utility import Excel
-class Test_Case1(BaseClass):
-
-    def test_script1(self):
-        data= Excel.get_data(self.XLPATH,'tc1',2,1)
-        print("Excel data",data)
-        print(self.driver.title)
-        print("end of execution")
