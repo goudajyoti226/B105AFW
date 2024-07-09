090032f95d331d0fe5346ffae5f8fa4cabd75122
goudajyoti226 <goudajyoti226@gmail.com>
2024-07-09 19:28:20 +0530

    new automated tc2

---
+++ test_scripts/test_invalid_login.py
@@ -0,0 +1,20 @@
+from generic.base_class import BaseClass
+from page.login_page import LoginPage
+
+
+class Test_InvalidLogin(BaseClass):
+
+    def test_invalid_login(self):
+        # 1. enter invalid username
+        login_page = LoginPage(self.driver)
+        login_page.set_username("abcd")
+
+        # 2. enter invalid password
+        login_page.set_password("xyz")
+
+        # 3. click on go button
+        login_page.click_go()
+
+        # 4. verify error msg is displayed
+        result=login_page.verify_errmsg_is_displayed(self.wait)
+        assert result
