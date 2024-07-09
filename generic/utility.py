ce284c0ae461270117605aa499039d82b7f66351
goudajyoti226 <goudajyoti226@gmail.com>
2024-07-09 20:26:16 +0530

    added multiple_invalid_login

--- generic/utility.py
+++ generic/utility.py
@@ -9,10 +9,20 @@ class Excel:
            if value==None:
                print("Cell value is NONE")
                value=""
-
       except Exception as e:
            print(str(e))
            value=""
 
       return value
 
+    @staticmethod
+    def get_rowcount(filepath,sheet_name):
+      try:
+           wb= openpyxl.load_workbook(filepath)
+           sheet=wb[sheet_name]
+           rc=sheet.max_row
+      except Exception as e:
+           print(str(e))
+           rc=0
+
+      return rc
